aa9df2356170d9e4ed3264737775bb05765bb70f
Alex Velasquez <jtinnesco@gmail.com>
2019-05-31 03:07:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+
+
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+ def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  
+end
